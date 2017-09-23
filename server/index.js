3e8908601fc53278d5b7710d984773b03f6e76d8
Andy Lindsay <andydlindsay@gmail.com>
2017-09-23 12:53:21 -0700

    adds dotenv and mongoose

--- server/index.js
+++ server/index.js
@@ -4,6 +4,13 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router = require('./routes/router');
+const mongoose = require('mongoose');
+
+// load environment variables
+require('dotenv').config();
+
+// database setup
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`);
 
 // app setup
 const app = express();
