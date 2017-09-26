9482e741e245d7f4d8c18927e642920a64dc01ea
Andy Lindsay <andydlindsay@gmail.com>
2017-09-25 21:43:09 -0700

    adds react starter app

--- server/router.js
+++ server/router.js
@@ -7,10 +7,12 @@ const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = (app) => {
 
+    // protected routes
     app.get('/', requireAuth, function(req, res, next) {
         res.send({ "hi": "there" });
     });
 
+    // signup and signin
     app.post('/signup', Authentication.signup);
     app.post('/signin', requireSignin, Authentication.signin);
 
